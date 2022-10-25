827304fee942d66882b4d315852b6bc1ff973b41
Helena Eckhardt <helenakeller92@gmail.com>
2022-10-25 19:25:30 +0200

    crash_course: first basic styling added

--- crash_course/lib/question.dart
+++ crash_course/lib/question.dart
@@ -7,6 +7,14 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: const TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
